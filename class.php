9901d291d48eca61e002801acec6b46fe1ee10ac
kartiks2000 <46376692+kartiks2000@users.noreply.github.com>
2020-08-08 11:12:00 +0530

    Add files via upload

---
+++ class.php
@@ -0,0 +1,17 @@
+<?php
+
+    class Car {
+
+        function carname() {
+            echo "MERCEDIES AMG Series...";
+        }
+
+    }
+
+    // Instantiating Class
+    $x = new Car();
+
+    // Calling the method on class instance
+    $x->carname();
+
+?>
